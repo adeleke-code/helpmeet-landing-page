94fca767e89445a8ebf64c41f16f023f0f665aae
akinyiliz <lizzieke84@gmail.com>
2023-11-29 14:12:15 +0300

    feat: Create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  primary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+  outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  outline,
+  disabled,
+  large,
+  fullWidth,
+  onClick,
+  primary,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+    disabled:opacity-70 
+    disabled:cursor-not-allowed 
+    rounded-2xl
+    font-semibold
+    hover:opacity-80
+    transition
+    
+    ${fullWidth ? "w-full" : "w-fit"}
+    ${primary ? "bg-[#009379]" : "bg-[#E5F4F2]"}
+    ${primary ? "text-white" : "text-[#009379]"}
+    ${large ? "px-8 py-2" : "px-4 py-2"}
+    ${outline ? "bg-transparent" : ""}
+    ${outline ? "border-2 border-[#009379]" : ""}
+    ${outline ? "text-[#009379]" : ""}
+    `}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
