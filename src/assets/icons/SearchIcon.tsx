d3c3adc829706932069b635c79e88b1401712c52
akinyiliz <lizzieke84@gmail.com>
2023-11-30 22:43:00 +0300

    style: Change stroke color and increase stroke width

--- src/assets/icons/SearchIcon.tsx
+++ src/assets/icons/SearchIcon.tsx
@@ -6,8 +6,8 @@ function SearchIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
+      strokeWidth={2.5}
+      stroke="white"
       className="w-6 h-6"
     >
       <path
