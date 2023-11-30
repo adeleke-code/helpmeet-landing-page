94b40b68e447b293259e369fee89b908366ca4d4
akinyiliz <lizzieke84@gmail.com>
2023-11-30 22:41:47 +0300

    style: Increase padding size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -30,11 +30,10 @@ const Button: React.FC<ButtonProps> = ({
     font-semibold
     hover:opacity-80
     transition
-    
     ${fullWidth ? "w-full" : "w-fit"}
     ${primary ? "bg-[#009379]" : "bg-[#E5F4F2]"}
     ${primary ? "text-white" : "text-[#009379]"}
-    ${large ? "px-8 py-2" : "px-4 py-2"}
+    ${large ? "px-12 py-2" : "px-8 py-2"}
     ${outline ? "bg-transparent" : ""}
     ${outline ? "border-2 border-[#009379]" : ""}
     ${outline ? "text-[#009379]" : ""}
