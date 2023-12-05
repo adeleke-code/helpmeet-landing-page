f8930d1f37f08986cf40e6cad5a59f9a4e87fd62
akinyiliz <lizzieke84@gmail.com>
2023-12-05 18:21:12 +0300

    feat: Add Categories, Hero, Crafts and Artisans sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Crafts from "./components/Crafts";
+import Artisans from "./components/Artisans";
+import Categories from "./components/Categories";
 
 function App() {
   return (
-    <div>
+    <div className="bg-[#F8F9FF] w-full h-full">
       <Navbar />
+      <Categories />
+      <Hero />
+      <Crafts />
+      <Artisans />
     </div>
   );
 }
