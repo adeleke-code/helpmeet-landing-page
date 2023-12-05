cfad98f25626d29c046b303c6b2c94bc76b6b9d6
akinyiliz <lizzieke84@gmail.com>
2023-12-05 17:56:57 +0300

    feat: Add artisans dummy data

---
+++ src/data/artisans.ts
@@ -0,0 +1,78 @@
+import car from "../assets/car.png";
+import plumber from "../assets/plumber.png";
+import hardware from "../assets/hardware.png";
+import technician from "../assets/technician.png";
+import engineer from "../assets/engineer.png";
+import mechanic from "../assets/mechanic.png";
+import welder from "../assets/welder.png";
+import pottery from "../assets/pottery.png";
+
+import avatar from "../assets/avatar.png";
+const artisans = [
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: car,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: plumber,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: hardware,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: technician,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: engineer,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: mechanic,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: welder,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Rewire",
+    username: "@taiwo_taiwo",
+    coverImage: pottery,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+];
+
+export default artisans;
