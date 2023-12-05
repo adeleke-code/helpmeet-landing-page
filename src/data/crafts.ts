bb7ab6d3bdc2742eb76ede0140ace5faf9234c55
akinyiliz <lizzieke84@gmail.com>
2023-12-05 17:57:38 +0300

    feat: Add crafts data

---
+++ src/data/crafts.ts
@@ -0,0 +1,19 @@
+import Auto from "../assets/icons/auto.png";
+import Design from "../assets/icons/design.png";
+import Catering from "../assets/icons/catering.png";
+import Lifestyle from "../assets/icons/lifestyle.png";
+import Renovation from "../assets/icons/renovation.png";
+import Photography from "../assets/icons/photography.png";
+import Construction from "../assets/icons/construction.png";
+
+const crafts = [
+  { icon: Construction, name: "Construction" },
+  { icon: Design, name: "Design & Craft" },
+  { icon: Catering, name: "Catering" },
+  { icon: Photography, name: "Photography" },
+  { icon: Renovation, name: "Renovation" },
+  { icon: Lifestyle, name: "Lifestyle & Beauty" },
+  { icon: Auto, name: "Auto" },
+];
+
+export default crafts;
