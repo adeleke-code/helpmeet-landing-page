48a46780872b18956cdc3bea8324361827834038
akinyiliz <lizzieke84@gmail.com>
2023-12-06 17:12:27 +0300

    feat: Add testimonials dummy data

---
+++ src/data/testimonials.ts
@@ -0,0 +1,29 @@
+import sarahAvatar from "../assets/sarah-avatar.png";
+import laurenAvatar from "../assets/lauren-avatar.png";
+import michaelAvatar from "../assets/michael-avatar.png";
+
+const testimonials = [
+  {
+    avatar: sarahAvatar,
+    name: "Sarah K.",
+    title: "UX Designer @Brello",
+    message:
+      "I was looking for a way to streamline my design process and Helpmeet was a lifesaver!",
+  },
+  {
+    avatar: michaelAvatar,
+    name: "Michael L.",
+    title: "Creative Director @Yo",
+    message:
+      "Helpmeet has been a game changer for my agency. The artisans have helped us deliver projects faster and with more consistency. Great job!",
+  },
+  {
+    avatar: laurenAvatar,
+    name: "Lauren M.",
+    title: "UI Designer @Boo",
+    message:
+      "Helpmeet has everything I need to get the job done quickly and efficiently.",
+  },
+];
+
+export default testimonials;
