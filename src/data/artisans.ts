238dfe942e58da80397d246b901def9e890f9a70
akinyiliz <lizzieke84@gmail.com>
2023-12-11 15:39:09 +0300

    feat: Add artisans dummy data

--- src/data/artisans.ts
+++ src/data/artisans.ts
@@ -1,74 +1,218 @@
 import car from "../assets/car.png";
-import welder from "../assets/welder.png";
+import baker from "../assets/baker.png";
+import avatar from "../assets/avatar.png";
+import tailor from "../assets/tailor.png";
 import plumber from "../assets/plumber.png";
 import pottery from "../assets/pottery.png";
-import hardware from "../assets/hardware.png";
-import engineer from "../assets/engineer.png";
-import mechanic from "../assets/mechanic.png";
-import technician from "../assets/technician.png";
+import scupltor from "../assets/sculptor.png";
+import shoeMaker from "../assets/shoe-maker.png";
+import beadMaker from "../assets/bead-maker.png";
+import farmWorker from "../assets/farm-worker.png";
+import ironBender from "../assets/iron-bender.png";
+import electrician from "../assets/electrician.png";
+import hairdresser from "../assets/hair-dresser.png";
+import truckDriver from "../assets/truck-driver.png";
+import paintArtist from "../assets/paint-artist.png";
+import photographer from "../assets/photographer.png";
+import musicTeacher from "../assets/music-teacher.png";
+import basketWeaver from "../assets/basket-weaver.png";
+import eventPlanner from "../assets/event-planner.png";
+import autoPanelBeater from "../assets/auto-panel.png";
+import dstvInstaller from "../assets/dstv-installer.png";
+import phoneRepairer from "../assets/phone-repairer.png";
+import interiorDecorator from "../assets/interior-deco.png";
+import buildingMaterials from "../assets/building-materials.png";
+import mechanicalEngineer from "../assets/mechanical-engineer.png";
 
-import avatar from "../assets/avatar.png";
 const artisans = [
   {
     name: "Rewire",
-    username: "@taiwo_taiwo",
+    username: "@enginebobo",
     coverImage: car,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
+    name: "Plumber",
+    username: "@pipefixers",
     coverImage: plumber,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
-    coverImage: hardware,
+    name: "Building Materials",
+    username: "@abokisupplies",
+    coverImage: buildingMaterials,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
-    coverImage: technician,
+    name: "DSTV Installer",
+    username: "@francisuzo",
+    coverImage: dstvInstaller,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
-    coverImage: engineer,
+    name: "Mechanical Engineer",
+    username: "@needforlights",
+    coverImage: mechanicalEngineer,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
-    coverImage: mechanic,
+    name: "Photographer",
+    username: "@bolagadebolakale",
+    coverImage: photographer,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
-    coverImage: welder,
+    name: "Music Teacher",
+    username: "@oyinbodele",
+    coverImage: musicTeacher,
     avatar: avatar,
     price: "N4200",
     rating: 24,
   },
   {
-    name: "Rewire",
-    username: "@taiwo_taiwo",
+    name: "Potter",
+    username: "@donvikki",
     coverImage: pottery,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Basket Weaver",
+    username: "@Hanatu-AsabeBaba",
+    coverImage: basketWeaver,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Bead Maker",
+    username: "@maasai_mara",
+    coverImage: beadMaker,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Electrician",
+    username: "@EleweduIdanGangan",
+    coverImage: electrician,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Event Planner",
+    username: "@native_eventsbookings",
+    coverImage: eventPlanner,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Interior Decorator",
+    username: "@Dele_Shawns",
+    coverImage: interiorDecorator,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Baker",
+    username: "@IbukunAlase",
+    coverImage: baker,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Hairdresser",
+    username: "@CinderellaElla",
+    coverImage: hairdresser,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Paint Artist",
+    username: "@BlessedJaga",
+    coverImage: paintArtist,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Phone Repairer",
+    username: "@getfixed",
+    coverImage: phoneRepairer,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Sculptor",
+    username: "@OluyaApoda",
+    coverImage: scupltor,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Shoe Maker",
+    username: "@OsonduDiallo",
+    coverImage: shoeMaker,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Tailor",
+    username: "@SisiAmunidara",
+    coverImage: tailor,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Auto Panel Beater",
+    username: "@OdionJago",
+    coverImage: autoPanelBeater,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Iron Bender",
+    username: "@Oluayebababendel",
+    coverImage: ironBender,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Truck Driver",
+    username: "@BabasDeloxe",
+    coverImage: truckDriver,
+    avatar: avatar,
+    price: "N4200",
+    rating: 24,
+  },
+  {
+    name: "Farm Worker",
+    username: "@@everythinggreen",
+    coverImage: farmWorker,
     avatar: avatar,
     price: "N4200",
     rating: 24,
