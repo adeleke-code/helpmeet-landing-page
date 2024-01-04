87c365264772cb42cb3ea800a0abba2e6045b519
akinyiliz <lizzieke84@gmail.com>
2024-01-04 17:07:18 +0300

    style: Refactor component width size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,54 +1,52 @@
-import React from "react";
-import Button from "./Button";
 import smilingMechanic from "../assets/smiling-mechanic.png";
 
 function About() {
   return (
-    <section className="h-full">
-      <div className="max-w-7xl mx-auto my-8 p-4">
+    <section className="h-full w-full xl:max-w-7xl xl:mx-auto">
+      <div className="my-8 p-4">
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 lg:mx-12">
           <div className="w-full md:w-1/2 flex flex-col justify-center text-[#2D2D2D]">
-            <h3 className="text-[26px] lg:text-[38px] font-bold ">
+            <h3 className="text-[23px] lg:text-3xl xl:text-4xl font-bold ">
               A <span className="text-[#009379]">lasting Solution</span>
               <br /> to your urgent needs
             </h3>
 
-            <div className="space-y-4">
-              <div className="border-l-4 border-l-[#009379] px-2 py-1 space-y-1">
-                <h4 className="text-base lg:text-lg text-[#009379] font-semibold">
+            <div className="space-y-2">
+              <div className="border-l-4 border-l-[#009379] px-2 space-y-1 lg:p-2">
+                <h4 className="text-base xl:text-lg text-[#009379] font-semibold">
                   Express quality service delivery
                 </h4>
-                <p className="text-xs lg:text-sm">
+                <p className="text-xs xl:text-sm">
                   Connect with the right artisan to deliver the services you
                   need within minutes.
                 </p>
               </div>
 
-              <div className="border-l-4 border-l-[#009379] p-2 space-y-1">
-                <h4 className="text-base lg:text-lg text-[#009379] font-semibold">
+              <div className="border-l-4 border-l-[#009379] px-2 space-y-1 lg:p-2">
+                <h4 className="text-base xl:text-lg text-[#009379] font-semibold">
                   Best for your budget
                 </h4>
-                <p className="text-xs lg:text-sm">
+                <p className="text-xs xl:text-sm">
                   Services are top rated and top vetted. Prices are best and
                   affordable, project based and competitive.
                 </p>
               </div>
 
-              <div className="border-l-4 border-l-[#009379] p-2 space-y-1">
-                <h4 className="text-base lg:text-lg text-[#009379] font-semibold">
+              <div className="border-l-4 border-l-[#009379] px-2 space-y-1 lg:p-2">
+                <h4 className="text-base xl:text-lg text-[#009379] font-semibold">
                   Secure payments
                 </h4>
-                <p className="text-xs lg:text-sm">
+                <p className="text-xs xl:text-sm">
                   All payments are secured. You know what you will pay upfront.
                   No payment is released until you approve the work.
                 </p>
               </div>
 
-              <div className="border-l-4 border-l-[#009379] p-2 space-y-1">
-                <h4 className="text-base lg:text-lg text-[#009379] font-semibold">
+              <div className="border-l-4 border-l-[#009379] px-2 space-y-1 lg:p-2">
+                <h4 className="text-base xl:text-lg text-[#009379] font-semibold">
                   24/7 support
                 </h4>
-                <p className="text-xs lg:text-sm">
+                <p className="text-xs xl:text-sm">
                   Got questions, queries, complaints or need information or
                   help? We are available 24/7 to attend to you, anywhere!
                 </p>
@@ -56,14 +54,18 @@ function About() {
             </div>
           </div>
 
-          <div className="w-full md:w-1/2 bg-[#BFE9F5] rounded-2xl space-y-12">
+          <div className="w-full md:w-1/2 rounded-t-2xl space-y-4">
             <img
               src={smilingMechanic}
               alt="Smiling mechanic with arms crossed holding a spanner"
               className="w-full"
             />
 
-            <Button label="Learn More" primary extraLarge />
+            {/* <Button label="Learn More" primary extraLarge /> */}
+
+            <button className="w-full bg-[#009379] text-[#F8F9FF] rounded-b-2xl py-3 font-semibold">
+              Learn More
+            </button>
           </div>
         </div>
       </div>
--- src/components/ArtisansSection.tsx
+++ src/components/ArtisansSection.tsx
@@ -10,12 +10,12 @@ function ArtisansSection() {
   const newArtisans = artisans.slice(0, 8);
 
   return (
-    <section className="h-full pb-8">
-      <div className="max-w-7xl mx-auto p-2 lg:p-4 my-8 grid grid-cols-2 md:grid-cols-4 gap-y-8 gap-2">
+    <section className="h-full w-full xl:max-w-7xl xl:mx-auto pb-8">
+      <div className="p-4 my-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-y-8 gap-2 md:gap-4 lg:gap-8">
         {newArtisans.map((artisan) => (
           <div
             key={artisan.coverImage}
-            className="lg:w-[90%] rounded-t-xl space-y-2"
+            className="w-full rounded-t-xl space-y-2"
           >
             <img
               src={artisan.coverImage}
