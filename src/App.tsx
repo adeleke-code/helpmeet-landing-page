aefcd45f8c5c679f71f858235de943451cc75531
akinyiliz <lizzieke84@gmail.com>
2024-01-04 17:03:38 +0300

    refactor: Remove Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Artisans from "./pages/Artisans";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="bg-[#F8F9FF] w-full h-full">
+    <div className="w-full h-full">
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/artisans" element={<Artisans />} />
